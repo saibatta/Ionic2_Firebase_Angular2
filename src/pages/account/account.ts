3a662c2de9e716a9190d5b4ca959b85ced4114fd
Sai Babu Batta <sbatta@mss.local>
2017-04-18 17:34:47 +0530

    checking user is online/offline status

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { AuthProvider } from '../../providers/auth-provider/auth-provider';
 import { UserProvider } from '../../providers/user-provider/user-provider';
+import { UtilProvider } from '../../providers/utils';
 
 @Component({
     templateUrl: 'account.html'
@@ -10,25 +11,36 @@ import { UserProvider } from '../../providers/user-provider/user-provider';
 export class AccountPage {
     rootNav;
     user = {};
-    constructor(public nav: NavController, 
-                public auth: AuthProvider, 
-                public userProvider: UserProvider,
-                public local:Storage) {
+    constructor(public nav: NavController,
+        public auth: AuthProvider,
+        public userProvider: UserProvider,
+        public local: Storage,
+        public util: UtilProvider) {
         this.userProvider.getUser()
-        .then(userObservable => {
-            userObservable.subscribe(user => {
-                this.user = user;
+            .then(userObservable => {
+                userObservable.subscribe(user => {
+                    this.user = user;
+                });
             });
-        });
     }
-    
+
     //save user info
     updatePicture() {
         this.userProvider.updatePicture();
     };
 
+    // logout() {
+    //     this.local.remove('uid');
+    //     this.auth.logout();
+    // }
+
     logout() {
-        this.local.remove('uid');
-        this.auth.logout();
-    }
+        this.auth.logout()
+            .then((data) => {
+                this.local.remove('uid');
+            }, (error) => {
+                let alert = this.util.doAlert("Error", error.message, "Ok");
+                alert.present();
+            });
+    };
 }
